b2ed05fb6d7dece91688d018804849e6123080d3
Alex Balli <alexanderjballi@gmail.com>
2024-04-01 16:44:50 -0700

    update: complete lesson

--- src/script.js
+++ src/script.js
@@ -18,29 +18,43 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/1.png')
 
 /**
  * Particles
  */
-
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
+const count = 50000
 
-const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
-for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+for(let i = 0; i < count * 3; i++)
 {
-    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
 // Material
-const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.1,
-    sizeAttenuation: true
-})
+const particlesMaterial = new THREE.PointsMaterial()
+
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+
+particlesMaterial.color = new THREE.Color('#ff88cc')
+
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.01
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+
+particlesMaterial.vertexColors = true
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
@@ -98,6 +112,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update particles
+    for(let i = 0; i < count; i++)
+    {
+        let i3 = i * 3
+
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
